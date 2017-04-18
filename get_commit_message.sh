744f65f2f4fe5c71053d7da2feae3fc1983b0905
Joe Loughry <joe.loughry@gmail.com>
2017-04-18 13:35:02 -0600

    Jekyll doesn't like symlinks; copy get_commit_message.sh here.

--- get_commit_message.sh
+++ get_commit_message.sh
@@ -1,1 +1,17 @@
-../Makefiles/get_commit_message.sh
+#!/bin/sh
+
+message="commit_message.txt"
+
+rm -f $message
+touch $message
+
+/bin/echo "Ready to commit; give me a message (it can be multiple lines); end with '.'"
+/bin/echo -n "> "
+
+read line
+while [ "$line" != "." ]
+do
+    echo $line >> $message
+    read line
+done
+
